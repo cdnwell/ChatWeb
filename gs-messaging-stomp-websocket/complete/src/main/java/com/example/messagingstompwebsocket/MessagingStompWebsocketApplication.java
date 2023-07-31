e3b5cf0be41e9df3c09ef746625573ec6e2e2141
cdnwell <cdnwellhk@gmail.com>
2023-07-31 15:17:24 +0900

    commit : re-commit

---
+++ gs-messaging-stomp-websocket/complete/src/main/java/com/example/messagingstompwebsocket/MessagingStompWebsocketApplication.java
@@ -0,0 +1,12 @@
+package com.example.messagingstompwebsocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// 19WW936#97
+@SpringBootApplication
+public class MessagingStompWebsocketApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(MessagingStompWebsocketApplication.class, args);
+	}
+}
